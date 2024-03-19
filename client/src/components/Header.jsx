a538ac28ab9e7ea4a49063274aac0c1482a4a98c
kaoutar <elazamikaoutar@gmail.com>
2024-03-19 20:46:41 +0000

    ajouter Header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <h1 className='font-bold text-sm'>
+            <span className='text-'></span>
+            <span></span>
+        </h1>
+    </header>
+  )
+}
