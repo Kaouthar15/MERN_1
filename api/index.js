c571c60143acc979a965e428f892033ed0dc055d
kaoutar <elazamikaoutar@gmail.com>
2024-03-20 15:41:01 +0000

    routes

--- api/index.js
+++ api/index.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from './routes/user.route.js';
 dotenv.config();
-const app = express();
-
 
 mongoose
-    .connect(process.env.MONGO) 
-    .then(()=>console.log('Connected to MONGO'))
-    .catch((err)=>console.log(err));
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to MONGO"))
+  .catch((err) => console.log(err));
+
+const app = express();
+
+app.listen(process.env.PORT, (err) => {
+  if (!err) {
+    console.log("Server is Running on port 3000");
+  } else {
+    console.log("Error on Running on port 3000");
+  }
+});
 
 
-app.listen(process.env.PORT,(err)=>{
-    if (!err)
-    {
-        console.log('Server is Running on port 3000');
-    }
-    else{
-        console.log('Error on Running on port 3000');
-    }
-})
+app.use('/api/user',userRouter);
