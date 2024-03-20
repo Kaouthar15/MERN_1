cfa16810809eb5645400a6d5189bb46a4ada5b29
kaoutar <elazamikaoutar@gmail.com>
2024-03-20 16:40:04 +0000

    add api auth

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -19,5 +20,18 @@ app.listen(process.env.PORT, (err) => {
   }
 });
 
+app.use(express.json());
 
 app.use('/api/user',userRouter);
+
+app.use('/api/auth',authRouter);
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
