c27bc0658b9c3d1d1a196d98fdd610861b6853ee
kaoutar <elazamikaoutar@gmail.com>
2024-05-07 22:51:16 +0100

    complete update listing functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import Profile from "./pages/Profile";
 import About from "./pages/about";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
+import UpdateListing from "./pages/UpdateListing";
+import CreateListing from "./pages/CreateListing";
 export default function App() {
   return (
     <BrowserRouter>
@@ -16,8 +18,13 @@ export default function App() {
         <Route path="/About" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route element={<PrivateRoute/>}>
+        <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
